976e31cd60efb14f3de97643f5a71265b47eba9e
Youusef Magdy <youssef.magdy.775@gmail.com>
2022-11-13 19:47:06 +0200

    edit the yaml file with another port

--- session_2/WpDockerInstall.sh
+++ session_2/WpDockerInstall.sh
@@ -16,21 +16,23 @@ if [[  "$dist" == "CentOS" ]]; then
     sudo docker --version
     sudo yum install docker-compose-plugin
 #install on ubuntu 
-elif [[ "$dist" == "Debian" || "$dist" == "Ubuntu"  ]]; then	
+elif [[ "$dist" == "Debian" || "$dist" == "Ubuntu"  ]]; then
+    if ! [ -x "$(command -v docker)" ]; then	
     sudo apt-get update
-    sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    sudo docker --version
-    sudo apt-get install docker-compose-plugin
+        sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+        sudo docker --version
+        sudo apt-get install docker-compose-plugin
+    fi
 fi 
 
 #install worpress 
@@ -39,5 +41,5 @@ cd ./my_wordpress/
 touch docker-compose.yml
 sudo apt install wget
 wget https://raw.githubusercontent.com/youssefmagdy1/devOps_training/master/session_2/docker-compose.yml -O  docker-compose.yml
-docker-compose up -d 
+sudo docker-compose up -d 
 
