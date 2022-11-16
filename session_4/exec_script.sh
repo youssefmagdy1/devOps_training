4f97ec8ebdd5c96fb07b580ff7d3204e8de261f3
Youusef Magdy <youssef.magdy.775@gmail.com>
2022-11-16 10:28:12 +0200

    session 4

--- session_4/exec_script.sh
+++ session_4/exec_script.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
+touch notify.txt
 
 tail -Fn0 logfile.log | \
 while read line ; do
-        echo $line 
         echo "$line" | grep  -q "Error"
         if [ $? = 0 ]
         then
-                echo "we found error"
+            echo "$(date) error found  $line" >> notify.txt 
         fi
 done
 
 # tail -f logfile.log | grep -C2 "Error"
+
 
 
